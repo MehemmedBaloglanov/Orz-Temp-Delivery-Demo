624204c805517379cf89f154437c7feea8f1a888
Rovshan Aghayev <rovsenaqazadeh@gmail.com>
2024-09-06 20:27:27 +0400

    order service structure initialized

---
+++ order-service/order-container/src/main/java/com/intellibucket/order/service/OrderServiceApplication.java
@@ -0,0 +1,7 @@
+package com.intellibucket.order.service;
+
+public class OrderServiceApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
