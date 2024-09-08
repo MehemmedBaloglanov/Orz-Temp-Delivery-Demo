31fcbd434c3f7de5bb0517d232a25eb5d23680e6
Rovshan Aghayev <rovsenaqazadeh@gmail.com>
2024-09-08 17:41:25 +0400

    cart service initialization

---
+++ cart-service/cart-domain/cart-domain-shell/src/main/java/com/intellibucket/cart/service/domain/shell/port/secondary/CartServiceRepository.java
@@ -0,0 +1,5 @@
+package com.intellibucket.cart.service.domain.shell.port.secondary;
+
+public interface CartServiceRepository {
+    void save();
+}
---
+++ cart-service/cart-secondary/cart-repository/src/main/java/com/intellibucket/cart/service/repository/mapper/package-info.java
@@ -0,0 +1,1 @@
+package com.intellibucket.cart.service.repository.mapper;
