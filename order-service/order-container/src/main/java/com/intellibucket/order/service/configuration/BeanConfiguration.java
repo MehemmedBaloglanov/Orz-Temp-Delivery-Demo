92a01ecdc9a7db50bdfc5639d9a4b37e4df09e6f
Ali Gadashov <gadashovali13@gmail.com>
2024-09-10 12:38:43 +0400

    Order Domain Service initialized

--- order-service/order-container/src/main/java/com/intellibucket/order/service/configuration/BeanConfiguration.java
+++ order-service/order-container/src/main/java/com/intellibucket/order/service/configuration/BeanConfiguration.java
@@ -1,8 +1,5 @@
 package com.intellibucket.order.service.configuration;
 
-import com.intellibucket.order.service.domain.core.service.OrderDomainService;
-import com.intellibucket.order.service.domain.shell.OrderDomainServiceImpl;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
--- order-service/order-domain/order-domain-core/src/main/java/com/intellibucket/order/service/domain/core/service/OrderDomainService.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/intellibucket/order/service/domain/core/service/OrderDomainService.java
@@ -1,4 +1,37 @@
 package com.intellibucket.order.service.domain.core.service;
 
+import com.intelliacademy.orizonroute.identity.company.CompanyID;
+import com.intelliacademy.orizonroute.identity.order.ord.OrderID;
+import com.intelliacademy.orizonroute.valueobjects.order.OrderNumber;
+import com.intellibucket.order.service.domain.core.event.*;
+import com.intellibucket.order.service.domain.core.root.OrderRoot;
+
+/**
+ * Author: Ali Gadashov
+ * Version: v1.0
+ * Date: 9/10/2024
+ * Time: 10:27 AM
+ */
+
 public interface OrderDomainService {
+
+    OrderCreatedEvent validateAndInitiateOrder(OrderRoot orderRoot, CompanyID companyID);
+
+    OrderPaidEvent  payOrder(OrderRoot orderRoot);
+
+    PaymentCanceledEvent cancelOrderPayment(OrderRoot orderRoot);
+
+    OrderAssignedEvent assignOrder(OrderRoot orderRoot);
+
+    OrderDeliveredEvent deliverOrder(OrderRoot orderRoot);
+
+    OrderPreparedEvent  orderPrepared(OrderRoot orderRoot);
+
+    OrderPreparingEvent prepareOrder(OrderRoot orderRoot);
+
+    OrderCancelEvent cancelOrder(OrderID orderID);
+
+    OrderRoot traceOrder(OrderNumber orderNumber);
+
+
 }
