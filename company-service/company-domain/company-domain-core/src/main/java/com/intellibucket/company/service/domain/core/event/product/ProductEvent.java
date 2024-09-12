36271adc695d41378239583d7b873442bdb1e08e
Mahammad Baloghlanov <mehemmedbaloglanov300@gmail.com>
2024-09-12 12:52:05 +0400

    add product event

---
+++ company-service/company-domain/company-domain-core/src/main/java/com/intellibucket/company/service/domain/core/event/product/ProductEvent.java
@@ -0,0 +1,16 @@
+package com.intellibucket.company.service.domain.core.event.product;
+
+import com.intellibucket.company.service.domain.core.root.ProductRoot;
+import com.intellibucket.event.DomainEvent;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.OffsetDateTime;
+
+
+@RequiredArgsConstructor
+@Getter
+public class ProductEvent implements DomainEvent<ProductRoot> {
+    private final ProductRoot productRoot;
+    private final OffsetDateTime createTime;
+}
