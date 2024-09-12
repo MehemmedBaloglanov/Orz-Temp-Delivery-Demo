679a6eedba6692b74adc47c74a9c5ebb54ee9491
OzlamHasanova <7qk9bme@code.edu.az>
2024-09-12 12:51:46 +0400

    add company event classes

--- company-service/company-domain/company-domain-core/src/main/java/com/intellibucket/company/service/domain/core/event/company/CompanyBannedEvent.java
+++ company-service/company-domain/company-domain-core/src/main/java/com/intellibucket/company/service/domain/core/event/company/CompanyBannedEvent.java
@@ -1,13 +1,12 @@
 package com.intellibucket.company.service.domain.core.event.company;
 
-
 import com.intellibucket.company.service.domain.core.root.CompanyRoot;
 
 import java.time.OffsetDateTime;
 
-public class CompanyCreatedEvent extends CompanyEvent {
+public class CompanyBannedEvent extends CompanyEvent{
 
-    public CompanyCreatedEvent(CompanyRoot companyRoot, OffsetDateTime createdTime) {
+    public CompanyBannedEvent(CompanyRoot companyRoot, OffsetDateTime createdTime) {
         super(companyRoot, createdTime);
     }
 }
--- company-service/company-domain/company-domain-core/src/main/java/com/intellibucket/company/service/domain/core/event/company/CompanyDisabledEvent.java
+++ company-service/company-domain/company-domain-core/src/main/java/com/intellibucket/company/service/domain/core/event/company/CompanyDisabledEvent.java
@@ -1,13 +1,11 @@
 package com.intellibucket.company.service.domain.core.event.company;
 
-
 import com.intellibucket.company.service.domain.core.root.CompanyRoot;
 
 import java.time.OffsetDateTime;
 
-public class CompanyCreatedEvent extends CompanyEvent {
-
-    public CompanyCreatedEvent(CompanyRoot companyRoot, OffsetDateTime createdTime) {
+public class CompanyDisabledEvent extends CompanyEvent{
+    public CompanyDisabledEvent(CompanyRoot companyRoot, OffsetDateTime createdTime) {
         super(companyRoot, createdTime);
     }
 }
