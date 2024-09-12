7dd3ebb84d4e17446cf86ea60105626fec84774c
OzlamHasanova <7qk9bme@code.edu.az>
2024-09-12 14:17:53 +0400

    update value objects

--- company-service/company-domain/company-domain-core/src/main/java/com/intellibucket/company/service/domain/core/valueobject/CompanyAddress.java
+++ company-service/company-domain/company-domain-core/src/main/java/com/intellibucket/company/service/domain/core/valueobject/CompanyAddress.java
@@ -2,14 +2,12 @@ package com.intellibucket.company.service.domain.core.valueobject;
 
 import com.intelliacademy.orizonroute.valueobjects.common.ValueObject;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @ValueObject
+@RequiredArgsConstructor
 @Getter
-public class CompanyAddress {
-    private String city;
-    private String street;
-    private String address;
-
+public record CompanyAddress(String city, String street, String address) {
     public Boolean isValid() {
 
         return city != null
