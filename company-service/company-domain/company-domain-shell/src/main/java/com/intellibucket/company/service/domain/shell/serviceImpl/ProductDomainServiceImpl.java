f799396c48983c65eeaa9653e4c8f46f12654000
Mahammad Baloghlanov <mehemmedbaloglanov300@gmail.com>
2024-09-12 15:07:17 +0400

    add productservice impl

---
+++ company-service/company-domain/company-domain-shell/src/main/java/com/intellibucket/company/service/domain/shell/serviceImpl/ProductDomainServiceImpl.java
@@ -0,0 +1,43 @@
+package com.intellibucket.company.service.domain.shell.serviceImpl;
+
+import com.intelliacademy.orizonroute.identity.company.CompanyID;
+import com.intellibucket.company.service.domain.core.event.product.*;
+import com.intellibucket.company.service.domain.core.root.ProductRoot;
+import com.intellibucket.company.service.domain.core.service.ProductDomainService;
+
+public class ProductDomainServiceImpl implements ProductDomainService {
+    @Override
+    public ProductCreatedEvent createProduct(ProductRoot productRoot, CompanyID companyID) {
+        return null;
+    }
+
+    @Override
+    public ProductDeletedEvent deleteProduct(ProductRoot productRoot, CompanyID companyID) {
+        return null;
+    }
+
+    @Override
+    public ProductActivatedEvent activateProduct(ProductRoot productRoot) {
+        return null;
+    }
+
+    @Override
+    public ProductOutOfStockEvent outOfStockProduct(ProductRoot productRoot) {
+        return null;
+    }
+
+    @Override
+    public ProductPriceUpdatedEvent priceUpdateProduct(ProductRoot productRoot) {
+        return null;
+    }
+
+    @Override
+    public ProductQuantityUpdatedEvent quantityUpdateProduct(ProductRoot productRoot) {
+        return null;
+    }
+
+    @Override
+    public ProductStockUpdatedEvent stockUpdateProduct(ProductRoot productRoot) {
+        return null;
+    }
+}
