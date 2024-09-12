ea00c2418d27d28659de42abcfed11cbafcb97d2
Mahammad Baloghlanov <mehemmedbaloglanov300@gmail.com>
2024-09-12 15:02:12 +0400

    add companyservice impl

---
+++ company-service/company-domain/company-domain-shell/src/main/java/com/intellibucket/company/service/domain/shell/serviceImpl/CompanyDomainServiceImpl.java
@@ -0,0 +1,35 @@
+package com.intellibucket.company.service.domain.shell.serviceImpl;
+
+import com.intelliacademy.orizonroute.identity.user.UserID;
+import com.intellibucket.company.service.domain.core.event.company.*;
+import com.intellibucket.company.service.domain.core.root.CompanyRoot;
+import com.intellibucket.company.service.domain.core.service.CompanyDomainService;
+import lombok.Builder;
+
+
+public class CompanyDomainServiceImpl implements CompanyDomainService {
+    @Override
+    public CompanyCreatedEvent createCompany(CompanyRoot companyRoot, UserID userID) {
+        return null;
+    }
+
+    @Override
+    public CompanyUpdatedEvent updateCompany(CompanyRoot companyRoot) {
+        return null;
+    }
+
+    @Override
+    public CompanyActivatedEvent activate(CompanyRoot companyRoot) {
+        return null;
+    }
+
+    @Override
+    public CompanyDisabledEvent disable(CompanyRoot companyRoot) {
+        return null;
+    }
+
+    @Override
+    public CompanyBannedEvent banned(CompanyRoot companyRoot) {
+        return null;
+    }
+}
