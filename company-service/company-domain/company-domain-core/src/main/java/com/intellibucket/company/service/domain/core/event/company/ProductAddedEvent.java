c87393d74fba590a819a1a0f918f2862feebc5cd
Mahammad Baloghlanov <mehemmedbaloglanov300@gmail.com>
2024-09-20 14:25:45 +0400

    refactor companyRoot

---
+++ company-service/company-domain/company-domain-core/src/main/java/com/intellibucket/company/service/domain/core/event/company/ProductAddedEvent.java
@@ -0,0 +1,16 @@
+package com.intellibucket.company.service.domain.core.event.company;
+
+import com.intellibucket.company.service.domain.core.root.CompanyRoot;
+import com.intellibucket.company.service.domain.core.root.ProductRoot;
+
+import java.time.OffsetDateTime;
+
+public class ProductAddedEvent extends CompanyEvent {
+
+    private final ProductRoot productRoot;
+
+    public ProductAddedEvent(CompanyRoot companyRoot, OffsetDateTime createdTime, ProductRoot productRoot) {
+        super(companyRoot, createdTime);
+        this.productRoot = productRoot;
+    }
+}
