427dc663d98b784aaaf240572a6bca603ae84a6f
Mahammad Baloghlanov <mehemmedbaloglanov300@gmail.com>
2024-09-20 15:53:37 +0400

    refactor companyRoot

--- company-service/company-domain/company-domain-core/src/main/java/com/intellibucket/company/service/domain/core/event/product/ProductPriceUpdatedEvent.java
+++ company-service/company-domain/company-domain-core/src/main/java/com/intellibucket/company/service/domain/core/event/product/ProductPriceUpdatedEvent.java
@@ -6,20 +6,8 @@ import com.intellibucket.company.service.domain.core.root.ProductRoot;
 import java.time.OffsetDateTime;
 
 public class ProductPriceUpdatedEvent extends ProductEvent{
-    private final Money oldPrice;
-    private final Money newPrice;
 
-    public ProductPriceUpdatedEvent(ProductRoot productRoot, Money oldPrice, Money newPrice) {
-        super(productRoot, OffsetDateTime.now());
-        this.oldPrice = oldPrice;
-        this.newPrice = newPrice;
-    }
-
-    public Money getOldPrice() {
-        return oldPrice;
-    }
-
-    public Money getNewPrice() {
-        return newPrice;
+    public ProductPriceUpdatedEvent(ProductRoot productRoot, OffsetDateTime createTime) {
+        super(productRoot, createTime);
     }
 }
