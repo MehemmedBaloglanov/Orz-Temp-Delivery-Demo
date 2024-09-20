a9537c15a3f03f2f20596c99348bce309abb8b88
gadashov <gadashovali13@gmail.com>
2024-09-20 14:46:23 +0400

    OrderCommand config

--- order-service/order-primary/order-rest/src/main/java/com/intellibucket/order/service/primary/rest/command/OrderCommandController.java
+++ order-service/order-primary/order-rest/src/main/java/com/intellibucket/order/service/primary/rest/command/OrderCommandController.java
@@ -4,6 +4,7 @@ import com.intelliacademy.orizonroute.identity.order.ord.OrderID;
 import com.intellibucket.order.service.domain.core.exception.OrderDomainException;
 import com.intellibucket.order.service.domain.shell.dto.rest.command.OrderAssignCommand;
 import com.intellibucket.order.service.domain.shell.dto.rest.command.OrderCancelCommand;
+import com.intellibucket.order.service.domain.shell.dto.rest.command.OrderRejectCommand;
 import com.intellibucket.order.service.domain.shell.dto.rest.response.OrderResponse;
 import com.intellibucket.order.service.domain.shell.port.input.rest.abstracts.command.OrderCommandServiceAdapter;
 import lombok.RequiredArgsConstructor;
@@ -24,15 +25,21 @@ public class OrderCommandController {
         OrderResponse order = orderCommandServiceAdapter.createOrder();
         return new ResponseEntity<>(order, HttpStatus.CREATED);
     }
+
+    //    TODO Customer yox Company e aiddi
+
     @GetMapping("/reject")
-    public ResponseEntity<OrderResponse> rejectOrder(@RequestBody OrderRejectCommand orderRejectCommand) {
-        OrderResponse response = orderCommandServiceAdapter.rejectOrder(orderRejectCommand);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<String> rejectOrder(@RequestBody OrderRejectCommand orderRejectCommand) throws OrderDomainException {
+        orderCommandServiceAdapter.rejectOrder(orderRejectCommand);
+        return ResponseEntity.ok("Order successfully assigned to agent.");
     }
+
+    //    TODO Customer yox Company e aiddi
+
     @PostMapping("/assign")
     public ResponseEntity<String> assignOrder(@RequestBody OrderAssignCommand orderAssignCommand) throws OrderDomainException {
         orderCommandServiceAdapter.assignOrder(orderAssignCommand);
-        return ResponseEntity.ok("Order successfully assigned to agent.");
+        return ResponseEntity.ok("Order successfully assigned.");
 
     }
     @PutMapping("/cancel")
