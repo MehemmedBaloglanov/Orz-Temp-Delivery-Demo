14c5567900d1aac4e3dc6a544dbfff5b1bfa14d2
Elshan Nabiyev <elshannabiyev98@gmail.com>
2024-09-30 15:02:26 +0400

    updated

--- user-service/user-domain/user-domain-shell/src/main/java/com/intellibucket/user/service/domain/shell/security/AbstractSecurityContextHolder.java
+++ user-service/user-domain/user-domain-shell/src/main/java/com/intellibucket/user/service/domain/shell/security/AbstractSecurityContextHolder.java
@@ -1,18 +1,18 @@
-package com.intellibucket.user.service.domain.shell.security;
-
-import com.intelliacademy.orizonroute.identity.company.CompanyID;
-import com.intelliacademy.orizonroute.identity.courier.CourierID;
-import com.intelliacademy.orizonroute.identity.customer.CustomerID;
-import com.intelliacademy.orizonroute.identity.user.UserID;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
-
-@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
-public interface AbstractSecurityContextHolder {
-    UserID currentUserID();
-    UserID currentNullableUserID();
-    CustomerID currentCustomerID();
-    CourierID currentCourierID();
-    CompanyID currentCompanyID();
-
-}
+//package com.intellibucket.user.service.domain.shell.security;
+//
+//import com.intelliacademy.orizonroute.identity.company.CompanyID;
+//import com.intelliacademy.orizonroute.identity.courier.CourierID;
+//import com.intelliacademy.orizonroute.identity.customer.CustomerID;
+//import com.intelliacademy.orizonroute.identity.user.UserID;
+//import org.springframework.context.annotation.Scope;
+//import org.springframework.context.annotation.ScopedProxyMode;
+//
+//@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+//public interface AbstractSecurityContextHolder {
+//    UserID currentUserID();
+//    UserID currentNullableUserID();
+//    CustomerID currentCustomerID();
+//    CourierID currentCourierID();
+//    CompanyID currentCompanyID();
+//
+//}
