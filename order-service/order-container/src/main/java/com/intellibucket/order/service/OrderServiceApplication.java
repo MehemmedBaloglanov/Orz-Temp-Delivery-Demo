6b709a4b327796665d484df94c956fcffc14105b
Mehmet Elibeyli <tomristt9@gmail.com>
2024-10-01 12:10:41 +0400

    added feignclient annotations to main class

--- order-service/order-container/src/main/java/com/intellibucket/order/service/OrderServiceApplication.java
+++ order-service/order-container/src/main/java/com/intellibucket/order/service/OrderServiceApplication.java
@@ -4,11 +4,13 @@ package com.intellibucket.order.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.intellibucket.order.service")
 @EnableJpaRepositories("com.intellibucket.order.service")
 @EntityScan("com.intellibucket.order.service")
+@EnableFeignClients
 
 public class OrderServiceApplication {
     public static void main(String[] args) {
