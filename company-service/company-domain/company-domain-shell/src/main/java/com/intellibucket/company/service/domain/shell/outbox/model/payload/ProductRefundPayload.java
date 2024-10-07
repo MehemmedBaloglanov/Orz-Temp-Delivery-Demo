0c63fac8a74cac1c9b74e234d82baa0b1af0b9db
Mahammad Baloghlanov <mehemmedbaloglanov300@gmail.com>
2024-10-07 17:15:55 +0400

    refactor companyRoot

---
+++ company-service/company-domain/company-domain-shell/src/main/java/com/intellibucket/company/service/domain/shell/outbox/model/payload/ProductRefundPayload.java
@@ -0,0 +1,20 @@
+package com.intellibucket.company.service.domain.shell.outbox.model.payload;
+
+import com.intellibucket.order.service.domain.core.valueobject.ApproveStatus;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductRefundPayload implements BaseEventPayload {
+    private UUID customerId;
+    private BigDecimal refundAmount;
+    private String failureMessage;
+    private Instant createdAt;
+}
