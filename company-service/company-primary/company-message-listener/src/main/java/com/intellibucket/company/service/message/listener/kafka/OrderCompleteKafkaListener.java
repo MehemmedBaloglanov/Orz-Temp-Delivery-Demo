09566f88a1d58c7020f2769390dff55970fe8977
Mahammad Baloghlanov <mehemmedbaloglanov300@gmail.com>
2024-10-07 17:41:25 +0400

    refactor companyRoot

--- company-service/company-primary/company-message-listener/src/main/java/com/intellibucket/company/service/message/listener/kafka/OrderCompleteKafkaListener.java
+++ company-service/company-primary/company-message-listener/src/main/java/com/intellibucket/company/service/message/listener/kafka/OrderCompleteKafkaListener.java
@@ -1,13 +1,15 @@
 package com.intellibucket.company.service.message.listener.kafka;
 
 import com.intellibucket.company.service.domain.core.exception.CompanyDomainException;
-import com.intellibucket.company.service.domain.shell.dto.message.order.refund.OrderCompanyRefundResponse;
-import com.intellibucket.company.service.domain.shell.port.input.listener.abstracts.AbstractOrderRefundResponseMessageListener;
+import com.intellibucket.company.service.domain.shell.dto.message.order.complete.OrderCompletedResponse;
+import com.intellibucket.company.service.domain.shell.port.input.listener.abstracts.AbstractOrderCompletedResponseMessageListener;
 import com.intellibucket.company.service.message.listener.mapper.CompanyMessagePublisherDataMapper;
 import com.intellibucket.kafka.config.consumer.KafkaConsumer;
 import com.intellibucket.kafka.order.avro.model.company.CompanyOrderRefundRequestAvroModel;
+import com.intellibucket.kafka.order.avro.model.company.OrderCompletedRequestAvroModel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -18,21 +20,22 @@ import java.util.List;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class ProductStockIncreaseKafkaListener implements KafkaConsumer<CompanyOrderRefundRequestAvroModel> {
+public class OrderCompleteKafkaListener implements KafkaConsumer<OrderCompletedRequestAvroModel>{
     private final CompanyMessagePublisherDataMapper companyMessagePublisherDataMapper;
-    private final AbstractOrderRefundResponseMessageListener orderRefundResponseMessageListener;
 
-    //todo burda niye orderCompanyRefundResponsu qebul elemir?
+    private final AbstractOrderCompletedResponseMessageListener  abstractOrderCompletedResponseMessageListener;
+
     @Override
-    public void receive(@Payload List<CompanyOrderRefundRequestAvroModel> messages,
+    public void receive(@Payload List<OrderCompletedRequestAvroModel> messages,
                         @Header(KafkaHeaders.RECEIVED_KEY) List<String> keys,
                         @Header(KafkaHeaders.RECEIVED_PARTITION) List<Integer> partitions,
                         @Header(KafkaHeaders.OFFSET) List<Long> offsets) {
-        messages.forEach(message -> {
-            OrderCompanyRefundResponse  orderCompanyRefundResponse = companyMessagePublisherDataMapper.companyOrderRefundRequestAvroModelToOrderRefundResponse(message);
+
+        messages.forEach(orderCompletedRequestAvroModel -> {
+            OrderCompletedResponse response = companyMessagePublisherDataMapper.orderCompletedRequestAvroModelToOrderCompletedResponse(orderCompletedRequestAvroModel);
             try{
-                orderRefundResponseMessageListener.refundOrder(orderCompanyRefundResponse);
-            }catch (CompanyDomainException e){
+                abstractOrderCompletedResponseMessageListener.completeOrder(response);
+            } catch (CompanyDomainException e) {
                 log.error(e.getMessage());
             }
         });
